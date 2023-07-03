808cf6c691e40faeb470adc872c43be05eec2e21
rootVIII <colleyloyejames@gmail.com>
2023-07-02 20:07:16 -0400

    initial

--- variables.tf
+++ variables.tf
@@ -32,7 +32,7 @@ variable "vpc_private_subnets" {
 variable "vpc_public_subnets" {
   description = "Public subnets for VPC"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
 variable "vpc_enable_nat_gateway" {
